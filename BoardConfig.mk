5f3013a2cae4ba1737f7728008f7b5595d527cfa
Isaac Chen <ycchen@google.com>
2017-10-24 15:09:36 +0800

    olivelite: Set TARGET_2ND_ARCH_VARIANT to armv8-a
    Setting TARGET_2ND_ARCH_VARIANT to armv8-a, instead of armv7-a-neon,
    can silence the related warning during build.
    
    Bug: 68012286

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@ DEVICE_PATH := device/xiaomi/olivelite
 
 # Architecture
 TARGET_ARCH := arm
-TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI  := armeabi-v7a
 TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := cortex-a53
