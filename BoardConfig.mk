c3d892c3385df15b9a1a5ebd4ba0c36b4828fc26
Louis Popi <theh2o64@gmail.com>
2020-07-25 08:38:14 +0200

    olivelite: Cleanup proprietary files

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,8 +40,6 @@ VENDOR_SECURITY_PATCH := 2020-05-01
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
 # HIDL
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
-DEVICE_MATRIX_FILE += $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
 # Kernel
